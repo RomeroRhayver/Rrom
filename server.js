81bff7221c4f34cfdedf61bde8d0e26c7f563fac
RomeroRhayver <romero.rhayverlloydjoseph.daguio@gmail.com>
2026-09-22 08:44:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-const express = require('express');
+const express = require("express");
+const Database = require("better-sqlite3");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+
 const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-
-// 1. Tell Express where to find your static assets (CSS, JS, Images)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// 2. Handle the homepage route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
-    // Adjust 'public' and 'index.html' to match your actual folder structure
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 const PORT = 3000;
 
